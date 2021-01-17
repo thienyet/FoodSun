29498376b8ae65d74d762776ed11565a6babe1d5
Bui Lua <luabui1198@gmail.com>
2021-01-17 08:19:51 +0700

    update

--- src/main/java/luabui/application/repository/AdminRepository.java
+++ src/main/java/luabui/application/repository/AdminRepository.java
@@ -2,6 +2,7 @@ package luabui.application.repository;
 
 import luabui.application.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     Admin findByEmail(String email);
 
+    @Query("select admin from Admin admin where admin.name like %:name%")
     List<Admin> findByName(String name);
     
 }
